3ca926a2b8a307f7a4a0758d681f59327b89735d
pasyaa <mhmdpsyh@gmail.com>
2026-04-07 14:55:42 +0700

    cout<< "Data ke - 3 yang baru :"<<dataku[2]<<endl;

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+    int dataku[5] = {10,20,30,40,50,};
+    int datamu[5];
+    int x;
+    
+    cout<< "Data ke - 3 :"<<dataku[2]<<endl;
+
+    x = dataku[2]*3;
+    dataku[2] = 600;
+    cout<< "Data ke - 3 yang baru :"<<dataku[2]<<endl;
+}
