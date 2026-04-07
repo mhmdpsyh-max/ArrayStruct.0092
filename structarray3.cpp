0dfd917dd80aefe0a0d022aa0ac3cb829765c4a9
pasyaa <mhmdpsyh@gmail.com>
2026-04-07 15:54:47 +0700

    cin >> mhs[i].umur;

--- structarray3.cpp
+++ structarray3.cpp
@@ -15,3 +15,24 @@ struct orang{
 int main()
 {
      orang mhs[3];
+     for (int i = 0; i <= 2; i++){
+    cout << "nama mahasiswa : " ;
+    getline(cin, mhs[i].nama);
+    cout << "alamat desa : " ;
+    getline(cin, mhs[i].alamat.desa);
+    cout << "umur : " ;
+    getline(cin, mhs[i].alamat.kota);
+    cout << "umur : ";
+    cin >> mhs[i].umur;
+    cout << endl;
+    }
+    cout <<endl;
+    
+    for (int i = 0; i <= 2; i++)
+    {
+    cout << "nama : " << mhs[i].nama<<endl;
+    cout << "desa : " << mhs[i].alamat.desa<<endl;
+    cout << "kota : " << mhs[i].alamat.kota<<endl;
+    cout << "umur : " << mhs[i].umur<<endl;
+    }
+}
